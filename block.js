98d9ae9e0e75aaa2150acdb352603e271a4e1682
tkAcharya <>
2021-01-23 00:09:59 +0530

    Added Block POJO class

---
+++ block.js
@@ -0,0 +1,9 @@
+class Block {
+
+  constructor({timestamp , lastHash , hash , data}) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+}
